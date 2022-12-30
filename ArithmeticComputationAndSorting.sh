3396d134f980f341655077896f40c81172a52358
Sachinmahajana <sachinmahajan782@gmail.com>
2022-12-30 22:43:33 +0530

    Added: Compute c + a/b

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,7 +1,8 @@
 echo "Welcome to Sorting Arithmetic Computation Program"
-!/bin/bash -x
+#!/bin/bash -x
 read -p "Enter first input :" a
 read -p "Enter second input:" b
 read -p "Enter third input :" c
 result1=$(($a + $b * $c ))
 result2=$(($(( $a * $b )) + $c ))
+result3=$(( $c + $(( $a / $b )) ))
