e1fef5b659de3c1f0986a55d14ea7f29d692fc84
Sachinmahajana <sachinmahajan782@gmail.com>
2022-12-30 23:09:16 +0530

    Added: Read value fom Dictonary into the array

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,8 +1,10 @@
 echo "Welcome to Sorting Arithmetic Computation Program"
-#!/bin/bash -x
+declare -A arithmaticOperation
+
 read -p "Enter first input :" a
 read -p "Enter second input:" b
 read -p "Enter third input :" c
+
 result1=$(($a + $b * $c ))
 result2=$(($(( $a * $b )) + $c ))
 result3=$(( $c + $(( $a / $b )) ))
@@ -12,4 +14,12 @@ arithmaticOperation[result1]=$result1
 arithmaticOperation[result2]=$result2
 arithmaticOperation[result3]=$result3
 arithmaticOperation[result4]=$result4
+#read the value dict to array
+
+for((i=0; i<=${#arithmaticOperation[@]}; i++))
+do
+  array[i]=${arithmaticOperation[result$((i+1))]}
+done
+#TO PRINT
+echo "${array[@]}"
 
